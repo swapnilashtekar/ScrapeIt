a6d757ee0e4ec0d45fbe0946442f03deac568bc5
Swapnil_from_Valyrian <swapashtekar@gmail.com>
2017-09-29 00:38:47 -0600

    Extracting text words from website

---
+++ find_all_text.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import urllib
+import logging
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)  
+    return u" ".join(t.strip() for t in visible_texts)
+
+
+
+html = urllib.urlopen(raw_input()).read()
+print(text_from_html(html))
